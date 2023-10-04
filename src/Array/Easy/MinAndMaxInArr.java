3d3077b6bd3d615d7e50a972f8b1e2dcd431f46f
MayankTiwari9 <mt1027319@gmail.com>
2023-10-05 00:00:15 +0530

    first commit

---
+++ src/Array/Easy/MinAndMaxInArr.java
@@ -0,0 +1,29 @@
+package Array.Easy;
+
+public class MinAndMaxInArr {
+    public static void minMaxSum(int[] arr){
+        int n = arr.length;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if(arr[i] > max)
+            max = arr[i];
+
+            if(arr[i] < min)
+            min = arr[i];
+        }
+
+        sum += min + max;
+
+        System.out.println("min -> " + min + " " + "max -> " + max);
+        System.out.println("Sum -> " + sum);
+        
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 4, 1};
+
+        minMaxSum(arr);
+    }
+}
---
+++ src/Array/Easy/ReverseTheArray.java
@@ -0,0 +1,25 @@
+package Array.Easy;
+
+public class ReverseTheArray {
+    public static void reverse(int arr[]){
+        int n = arr.length;
+        int left = 0;
+        int right = n-1;
+
+        while(left <= right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {4,5,1,2};
+
+        reverse(arr);
+    }
+}
